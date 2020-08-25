f9d6e03dfbde46d5cce8a051933684c2f4e15d53
Josuéflx <josueflx1999@gmail.com>
2020-08-24 23:11:04 -0300

    Criação MainViewController
    Implementando Initializable
    Declarando atributos
    Implementando metodos que gerenciand os event handler dos componentes
    Implementando metodo initialize

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemSeller;
+	@FXML
+	private MenuItem menuItemDepartment;
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	public void onMenuItemSellerAction() { // event handler
+		System.out.println("onMenuItemSellerAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartmentAction() { // event handler
+		System.out.println("onMenuItemDepartmentAction");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() { // event handler
+		System.out.println("onMenuItemAboutAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	}
+}
