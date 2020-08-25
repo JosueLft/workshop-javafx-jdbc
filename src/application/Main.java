0dc364f7e3cb46035860a0405d5dfbb02e539cd1
Josuéflx <josueflx1999@gmail.com>
2020-08-24 22:50:53 -0300

    Criando arquivo fxml MainView Alterando metodo start na classe main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,28 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); // instanciando o objeto com o caminho da view
+			Parent parent = loader.load(); // carregando a view
+			Scene mainScene = new Scene(parent); // instanciando a cena passando o objeto
+			primaryStage.setScene(mainScene); // setando a cena especifica como cena principal
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
